a5738644199e3a68af3815e4823facf5e721bd37
rezunenko-yurii <yuriy.rezunenko@gmail.com>
2021-04-06 15:08:16 +0300

    06.04.2021 15:08:16

--- Assets/IvoryFoxPackagesHelper/Editor/Scripts/IvoryFoxPackagesInstaller.cs
+++ Assets/IvoryFoxPackagesHelper/Editor/Scripts/IvoryFoxPackagesInstaller.cs
@@ -2,22 +2,22 @@ using UnityEditor;
 using UnityEditor.PackageManager;
 using UnityEngine;
 
-namespace IvoryFoxPackagesHelper.Scripts
+namespace Assets.IvoryFoxPackagesHelper.Editor.Scripts
 {
     public static class IvoryFoxPackagesInstaller
     {
         private const string PackageName = "https://github.com/rezunenko-yurii/ivoryfox-stable.git?path=Assets/IvoryFoxPackages";
-    
+
         [MenuItem("IvoryFox/Packages/Install Package Manager")]
         public static void Install()
         {
             var request = Client.Add(PackageName);
- 
+
             while (!request.IsCompleted)
             {
                 System.Threading.Tasks.Task.Delay(100);
             }
- 
+
             if (request.Status != StatusCode.Success)
             {
                 Debug.LogError("Cannot import " + PackageName + ": " + request.Error.message);
